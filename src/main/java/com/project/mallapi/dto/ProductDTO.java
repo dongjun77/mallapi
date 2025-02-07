faf8256b54ba239e4f8e937967a09c35c9ef5b43
dongjun <edj0707@naver.com>
2025-02-07 16:41:04 +0900

    feat: product DTO, CustomFileUtil

---
+++ src/main/java/com/project/mallapi/dto/ProductDTO.java
@@ -0,0 +1,33 @@
+package com.project.mallapi.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDTO {
+
+    private Long pno;
+
+    private String pname;
+
+    private int price;
+
+    private String pdesc;
+
+    private boolean defFlag;
+
+    @Builder.Default
+    private List<MultipartFile> files = new ArrayList<>(); //
+
+    @Builder.Default
+    private List<String> uploadedFileNames = new ArrayList<>(); // 파일의 이름들
+
+}
