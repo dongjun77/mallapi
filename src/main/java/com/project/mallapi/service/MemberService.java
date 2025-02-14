e9a8bc47136ba99392d4e844253623e97208fd23
dongjun <edj0707@naver.com>
2025-02-14 21:21:37 +0900

    feat: api에서 소셜토큰으로 소셜 회원 처리

--- src/main/java/com/project/mallapi/service/MemberService.java
+++ src/main/java/com/project/mallapi/service/MemberService.java
@@ -1,11 +1,25 @@
 package com.project.mallapi.service;
 
+import com.project.mallapi.domain.Member;
 import com.project.mallapi.dto.MemberDTO;
 import jakarta.transaction.Transactional;
+import java.util.stream.Collectors;
 
 @Transactional
 public interface MemberService {
 
     MemberDTO getKakaoMember(String accessToken);
+
+    default MemberDTO entityToDTO(Member member) {
+
+        MemberDTO dto = new MemberDTO(
+                member.getEmail(),
+                member.getPw(),
+                member.getNickname(),
+                member.isSocial(),
+                member.getMemberRoleList().stream().map(memberRole -> memberRole.name()).collect(Collectors.toList())
+        );
+        return dto;
+    }
 
 }
--- src/main/java/com/project/mallapi/service/MemberServiceImpl.java
+++ src/main/java/com/project/mallapi/service/MemberServiceImpl.java
@@ -1,14 +1,18 @@
 package com.project.mallapi.service;
 
+import com.project.mallapi.domain.Member;
+import com.project.mallapi.domain.MemberRole;
 import com.project.mallapi.dto.MemberDTO;
 import com.project.mallapi.repository.MemberRepository;
 import java.util.LinkedHashMap;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponents;
@@ -21,19 +25,56 @@ public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
 
+    private final PasswordEncoder passwordEncoder;
+
     @Override
     public MemberDTO getKakaoMember(String accessToken) {
 
+        // 카카오 연동 닉네임 -- 이메일 주소에 해당
         // accessToken을 이용해서 사용자 정보 가져오기
-
-        getEmailFromKakaoAccessToken(accessToken);
+        String nickname = getEmailFromKakaoAccessToken(accessToken);
 
         // 기존에 DB에 회원 정보가 있는 경우 / 없는 경우
 
-        return null;
+        Optional<Member> result = memberRepository.findById(nickname);
+
+        if(result.isPresent()) {
+
+            MemberDTO memberDTO = entityToDTO(result.get());
+
+            log.info("existed................." + memberDTO);
+
+            return memberDTO;
+            //return
+        }
+
+        Member socialMember = makeSocialMember(nickname);
+
+        memberRepository.save(socialMember);
+
+        MemberDTO memberDTO = entityToDTO(socialMember);
+
+        return memberDTO;
     }
 
-    private void getEmailFromKakaoAccessToken(String accessToken){
+    private Member makeSocialMember(String email) {
+        String tempPassword = makeTempPassword();
+
+        log.info("tempPassword:"+tempPassword);
+
+        Member member = Member.builder()
+                .email(email)
+                .pw(passwordEncoder.encode(tempPassword))
+                .nickname("Social Member")
+                .social(true)
+                .build();
+
+        member.addRole(MemberRole.USER);
+
+        return member;
+    }
+
+    private String getEmailFromKakaoAccessToken(String accessToken){
 
         String kakaoGetUserURL = "https://kapi.kakao.com/v2/user/me";
 
@@ -54,8 +95,22 @@ public class MemberServiceImpl implements MemberService {
 
         LinkedHashMap<String, LinkedHashMap> bodyMap = response.getBody();
 
-        log.info("-----------------------------------");
-        log.info(bodyMap);
+        LinkedHashMap<String, String> kakaoAccount = bodyMap.get("properties");
+        log.info("kakaoAccount: " + kakaoAccount);
 
+        String nickname = kakaoAccount.get("nickname");
+        log.info("nickname: " + nickname);
+
+        return nickname;
+
+    }
+
+    private String makeTempPassword() {
+        StringBuffer buffer = new StringBuffer();
+
+        for(int i=0; i<10; i++){
+            buffer.append((char)((int)(Math.random()*55)+65));
+        }
+        return buffer.toString();
     }
 }
